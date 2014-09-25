bb898fc455bc1b5a37030604120317d7574f9ce4
Dmitry Myasnikov <saver_is_not@bk.ru>
2014-09-25 06:58:55 +0400

    Script for dependency installation added

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ JTHarnessSimpleTest
 ===================
 
 Simple test project, that builds with Maven.
+
+You should have Maven installed in ypu system.
+
+1) ./install_dependency.sh
+2) mvn prepare-package
+3) mvn exec:exec
--- install_dependency.sh
+++ install_dependency.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+wget -c http://download.java.net/jtharness/4.4.1/Rel/jtharness-4_4_1-MR1-bin-b13-20_dec_2011.zip
+unzip jtharness-4_4_1-MR1-bin-b13-20_dec_2011.zip -d /tmp/jtharness-4.4.1
+mkdir -p lib
+mv -f /tmp/jtharness-4.4.1/lib/javatest.jar lib/
+mv -f /tmp/jtharness-4.4.1/lib/jh.jar lib/
+rm -rf /tmp/jtharness-4.4.1
 mvn install:install-file -DgroupId=com.sun -DartifactId=javatest -Dversion=4.4.1 -Dpackaging=jar -Dfile=./lib/javatest.jar
