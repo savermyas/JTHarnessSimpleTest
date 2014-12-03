aafca089edb0887f592dc0e640b8b95977558633
savermyas <saver_is_not@bk.ru>
2014-12-03 18:34:36 +0300

    Added javatest wrapper class

---
+++ src/main/java/com/oracle/simpletest/MyJavaTest.java
@@ -0,0 +1,12 @@
+package com.oracle.simpletest;
+
+import com.sun.javatest.tool.Main;
+
+public class MyJavaTest extends com.sun.javatest.tool.Main {
+	
+	public static void main(String[] args) {
+		Main m = new Main();
+		m.main(args);
+	}
+	
+}
