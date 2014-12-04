01b09f02b6995806a375f92e3014a2b44a25804b
savermyas <saver_is_not@bk.ru>
2014-12-04 13:17:52 +0300

    added template test suite and interview

---
+++ src/main/java/com/oracle/simpletest/MySimpleTestInterview.java
@@ -0,0 +1,12 @@
+package com.oracle.simpletest;
+
+import com.sun.interview.Interview;
+
+public class MySimpleTestInterview extends Interview {
+
+	protected MySimpleTestInterview(Interview parent, String baseTag) {
+		super(parent, baseTag);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/com/oracle/simpletest/MySimpleTestSuite.java
@@ -0,0 +1,16 @@
+package com.oracle.simpletest;
+
+import java.io.File;
+import java.util.Map;
+
+import com.sun.tck.j2me.javatest.J2meBaseTestSuite;
+
+public class MySimpleTestSuite extends J2meBaseTestSuite {
+
+	public MySimpleTestSuite(File root, Map tsInfo, ClassLoader cl)
+			throws Fault {
+		super(root, tsInfo, cl);
+		// TODO Auto-generated constructor stub
+	}
+
+}
